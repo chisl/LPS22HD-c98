3aaf1847cf430f9a77c6e9639aceb0412798751b
chisl <info@chisl.io>
2018-01-09 11:21:20 +0100

    Initial commit

---
+++ LPS22HD.c
@@ -0,0 +1,15 @@
+/*
+ * name:        LPS22HD
+ * description: MEMS nano pressure sensor: 260-1260 hPa absolute digital output barometer
+ * manuf:       STMicroelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/lps22hd.pdf
+ * date:        2018-01-05
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+
